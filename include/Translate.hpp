b20eb3157d6d0073c6c7bb3e75728df59f3e2cf3
Daniel Wiberg <d.a.wiberg@gmail.com>
2011-03-03 23:47:38 +0100

    Added license information.

--- include/Translate.hpp
+++ include/Translate.hpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010 Daniel Wiberg
+ * Copyright (c) 2011 Daniel Wiberg
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
